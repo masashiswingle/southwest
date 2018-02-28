2bc48476f1441da993f069d3ce0af6ee261877b6
Masashi Swingle <masashiswingle@hotmail.com>
2018-02-27 21:56:16 -0800

    Added .gitignore BC

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 6969; 
-var bodyParser = require('body-parser');
-var southwest = require('./southwest');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 6969; 
+const bodyParser = require('body-parser');
+const southwest = require('./southwest');
 
 app.disable('x-powered-by'); //don't flaunt express
 
@@ -10,13 +11,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); //parse json and www-form-urlencoded
 
 app.use('/', express.static('public'));
+app.get('/creator',(req, res) =>{
+	res.sendFile(path.join(__dirname, 'public/creator/creatorView.html'))
+})
 // app.get('/', function (req, res){
 // 	return res.status(200).send('pong')
 // })
 
 app.use('/api', southwest);
 
-app.listen(port, function(){
+app.listen(port, () => {
 	console.log('Server is listening on http://localhost:' + port)
 });
 
